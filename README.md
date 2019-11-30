8c3f1546301a484414ee3f8ad138d8d4ae25213b
Nils Jørgen Mittet <njmittet@gmail.com>
2019-11-30 21:55:11 +0100

    Add React to a website
    Create example file demonstrating usage of non-JSX React added directly to a website.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # learning-react
 Notes and code written while learning React and Redux.
 
+Serve the example files (in `.public/`) by executing `npx light-server -s public -p 8080  -w "public/**"` from the command line.
+
 ## Notes
 [React and Redux](notes/ReactRedux.md) code examples created while learning from sources like [freeCodeCamp.org](https://www.freecodecamp.org/learn/).
 
---
+++ public/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// See https://reactjs.org/docs/react-without-jsx.html for syntax explanation.
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'Clicked!';
+    }
+
+    return e('button', { onClick: () => this.setState({ liked: true }) }, 'Click Me');
+  }
+}
+
+ReactDOM.render(e(LikeButton), document.querySelector('#click'));
