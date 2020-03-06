2cdb39c2686bc12bfb5aba1a81f7b124a02b4451
Nils Jørgen Mittet <njmittet@gmail.com>
2020-03-06 13:35:17 +0100

    Add Prettier to project
    - Updated ESLint-related plugins.
    - Installed Prettier and plugins for ESLint interop.
    - Added configuration file for Prettier.
    - Configured ESLint to work with Prettier.
    
    Resources:
    https://www.robinwieruch.de/prettier-eslint
    https://github.com/prettier/eslint-config-prettier
    https://github.com/sheerun/prettier-standard

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 class LikeButton extends React.Component {
   constructor(props) {
@@ -8,11 +8,11 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return "Liked!";
+      return 'Liked!';
     }
 
     return <button onClick={() => this.setState({ liked: true })}>Like</button>;
   }
 }
 
-ReactDOM.render(<LikeButton />, document.querySelector("#click"));
+ReactDOM.render(<LikeButton />, document.querySelector('#click'));
